e5c5fa60714a3a702af785c971742cb7d2bc4309
itsmartashub <44645238+itsmartashub@users.noreply.github.com>
2025-09-09 16:21:52 +0200

    feat(background): Add toolbar extension icon badge with current ext version and accent bg

--- entrypoints/background.js
+++ entrypoints/background.js
@@ -1,3 +1,19 @@
+import { browser } from '#imports'
+
 export default defineBackground(() => {
-	console.log('Hello background!', { id: browser.runtime.id })
+	async function updateBadge() {
+		const manifest = browser.runtime.getManifest()
+		const badgeText = manifest.version
+
+		browser.action.setBadgeText({ text: badgeText })
+		browser.action.setBadgeBackgroundColor({ color: '#adb4e4' })
+	}
+
+	// Update badge on installation/update
+	browser.runtime.onInstalled.addListener(async () => {
+		await updateBadge()
+	})
+
+	// Initial badge setup
+	updateBadge()
 })
